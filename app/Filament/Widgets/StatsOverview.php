656527a759c891be43a82bcae7330aca102ea720
diskonnekted <arif.susilo@gmail.com>
2026-04-22 06:24:05 +0700

    style: add decorative SVG background icons to dashboard stat cards

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -24,37 +24,43 @@ class StatsOverview extends StatsOverviewWidget
                 ->descriptionIcon('heroicon-m-document-text')
                 ->color('success')
                 ->chart([7, 3, 4, 5, 6, 3, 5])
-                ->chartColor('success'),
+                ->chartColor('success')
+                ->extraAttributes(['class' => 'stat-card stat-card--docs']),
             Stat::make('Total Berita', number_format($totalNews))
                 ->description('Berita & artikel terbit')
                 ->descriptionIcon('heroicon-m-newspaper')
                 ->color('info')
                 ->chart([3, 5, 2, 7, 4, 6, 5])
-                ->chartColor('info'),
+                ->chartColor('info')
+                ->extraAttributes(['class' => 'stat-card stat-card--news']),
             Stat::make('Jenis Produk Hukum', number_format($totalCategories))
                 ->description('Kategori peraturan')
                 ->descriptionIcon('heroicon-m-tag')
                 ->color('warning')
                 ->chart([2, 3, 4, 5, 4, 3, 5])
-                ->chartColor('warning'),
+                ->chartColor('warning')
+                ->extraAttributes(['class' => 'stat-card stat-card--categories']),
             Stat::make('Banner Aktif', number_format($totalBanners))
                 ->description('Banner hero aktif')
                 ->descriptionIcon('heroicon-m-photo')
                 ->color('danger')
                 ->chart([1, 1, 2, 1, 2, 1, 1])
-                ->chartColor('danger'),
+                ->chartColor('danger')
+                ->extraAttributes(['class' => 'stat-card stat-card--banners']),
             Stat::make('Anggota JDIH', number_format($totalMembers))
                 ->description('Anggota terdaftar')
                 ->descriptionIcon('heroicon-m-users')
                 ->color('success')
                 ->chart([4, 5, 6, 7, 6, 8, 9])
-                ->chartColor('success'),
+                ->chartColor('success')
+                ->extraAttributes(['class' => 'stat-card stat-card--members']),
             Stat::make('Total Unduhan', number_format($totalDownloads))
                 ->description('File dapat diunduh')
                 ->descriptionIcon('heroicon-m-arrow-down-tray')
                 ->color('info')
                 ->chart([5, 3, 4, 6, 5, 7, 8])
-                ->chartColor('info'),
+                ->chartColor('info')
+                ->extraAttributes(['class' => 'stat-card stat-card--downloads']),
         ];
     }
 }
