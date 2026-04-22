67d8fdc32b262ff4aa47e957be4d4e8dbe69c13f
diskonnekted <arif.susilo@gmail.com>
2026-04-22 09:57:25 +0700

    fix: populate VisitorChart with mock data to ensure visibility on dashboard

--- app/Filament/Widgets/VisitorChart.php
+++ app/Filament/Widgets/VisitorChart.php
@@ -15,7 +15,17 @@ class VisitorChart extends ChartWidget
     protected function getData(): array
     {
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Pengunjung Situs',
+                    'data' => [450, 590, 800, 810, 560, 550, 780],
+                    'fill' => 'start',
+                    'borderColor' => '#003399',
+                    'backgroundColor' => 'rgba(0, 51, 153, 0.1)',
+                    'tension' => 0.4,
+                ],
+            ],
+            'labels' => ['Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab', 'Min'],
         ];
     }
 
