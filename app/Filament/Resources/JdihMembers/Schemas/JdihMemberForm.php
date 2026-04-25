e5129f5804b4d920a123055f359cf38b02f063aa
diskonnekted <arif.susilo@gmail.com>
2026-04-25 22:52:52 +0700

    Fix: Finalize all upload disks including JdihMembers

--- app/Filament/Resources/JdihMembers/Schemas/JdihMemberForm.php
+++ app/Filament/Resources/JdihMembers/Schemas/JdihMemberForm.php
@@ -22,6 +22,7 @@ class JdihMemberForm
                 FileUpload::make('photo_path')
                     ->label('Foto Profil')
                     ->image()
+                    ->disk('public')
                     ->directory('members')
                     ->required(),
             ]);
