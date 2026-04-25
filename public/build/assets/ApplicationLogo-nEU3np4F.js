7df88bc9556acfdaeaf403a42ad51cc00319ec74
diskonnekted <arif.susilo@gmail.com>
2026-04-26 06:48:43 +0700

    build: finalized assets for production

---
+++ public/build/assets/ApplicationLogo-nEU3np4F.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-DyqjkIu9.js";function n(a){return o.jsx("img",{...a,src:"/images/logo-jdih.png",alt:"Logo JDIH Banjarnegara",className:`h-20 w-auto object-contain ${a.className||""}`})}export{n as A};
