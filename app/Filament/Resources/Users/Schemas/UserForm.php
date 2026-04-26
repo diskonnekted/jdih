6544136d0ed0425a82be44f071dbcef85c88617f
diskonnekted <arif.susilo@gmail.com>
2026-04-26 07:47:24 +0700

    fix: add fields to user management form

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -10,7 +10,22 @@ class UserForm
     {
         return $schema
             ->components([
-                //
+                \Filament\Forms\Components\TextInput::make('name')
+                    ->label('Nama')
+                    ->required()
+                    ->maxLength(255),
+                \Filament\Forms\Components\TextInput::make('email')
+                    ->label('Email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true),
+                \Filament\Forms\Components\TextInput::make('password')
+                    ->label('Kata Sandi')
+                    ->password()
+                    ->required(fn ($livewire) => $livewire instanceof \Filament\Resources\Users\Pages\CreateUser)
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->maxLength(255),
             ]);
     }
 }
