5f9224e6d01c62f062352acf54d3e355b53e8d76
diskonnekted <arif.susilo@gmail.com>
2026-04-26 07:39:43 +0700

    build: production assets with welcome video fix

---
+++ public/build/assets/ApplicationLogo-_AazYiUW.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-C5Mr5jrx.js";function n(a){return o.jsx("img",{...a,src:"/images/logo-jdih.png",alt:"Logo JDIH Banjarnegara",className:`h-20 w-auto object-contain ${a.className||""}`})}export{n as A};
